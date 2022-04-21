a03cfd8e19ed3db60067508539ef6c5eb47e3d02
Alex <oas90@bk.ru>
2022-04-21 13:03:56 +0300

    fix migration

--- database/migrations/2022_04_21_070555_change_to_null_postcode_column_by_default_in_delivery_addresses_table.php
+++ database/migrations/2022_04_21_070555_change_to_null_postcode_column_by_default_in_delivery_addresses_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Doctrine\DBAL\Types\StringType;
+use Doctrine\DBAL\Types\Type;
 
 class ChangeToNullPostcodeColumnByDefaultInDeliveryAddressesTable extends Migration
 {
