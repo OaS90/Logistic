49a967d1458e98d5c63a452ecfb193168f2bd353
Alex <oas90@bk.ru>
2022-04-29 10:17:05 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,5 @@ Route::post('import-app', [ApplicationController::class, 'import']);
 Route::get('application/{id}/sticker', [ApplicationController::class, 'makeSticker'])->name('make-sticker');
 
 Route::get('test', function () {
-    \Illuminate\Support\Facades\Mail::to('oas90@bk.ru')
-        ->from('lk.bortudachi.ru')->send(new TestMail());
+    \Illuminate\Support\Facades\Mail::to('oas90@bk.ru')->send(new TestMail());
 });
