24a644ca4d053b9ec299916694adc5ca8b9bd8e7
Alex <oas90@bk.ru>
2022-06-09 17:13:59 +0300

    another fix for updating quotes

--- app/Infrastructure/Repositories/Admin/QuoteRepository.php
+++ app/Infrastructure/Repositories/Admin/QuoteRepository.php
@@ -16,8 +16,8 @@ class QuoteRepository
             $quoteEntry->update([
                 'quote' => $quote['quote'],
                 'tmp_quote' => $quote['tmp_quote'] ?? null,
-                'available_from_date' => $quote['tmp_date'][0] ? Carbon::parse(['tmp_date'][0])->format('Y-m-d') : null,
-                'available_until_date' => $quote['tmp_date'][1]? Carbon::parse(['tmp_date'][1])->format('Y-m-d') : null,
+                'available_from_date' => isset($quote['tmp_date'][0]) ? Carbon::parse(['tmp_date'][0])->format('Y-m-d') : null,
+                'available_until_date' => isset($quote['tmp_date'][1]) ? Carbon::parse(['tmp_date'][1])->format('Y-m-d') : null,
             ]);
 
             $updatedQuotes = $quoteEntry;
