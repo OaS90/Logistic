3cc4b7ab75dbc771024d761cbd30cb27a1c38d35
Alex <oas90@bk.ru>
2022-06-27 10:43:01 +0300

    fix sidebar

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -4,6 +4,7 @@
     <li class='nav-item'><a class='nav-link' href='{{ backpack_url('partners') }}'><i class="las la-user-tie"></i> Партнёры</a></li>
     <li class='nav-item'><a class='nav-link' href='{{ backpack_url('applications') }}'><i class="las la-file-alt"></i> Заявки</a></li>
     <li class='nav-item'><a class='nav-link' href='{{ backpack_url('warehouses') }}'><i class="las la-file-alt"></i> Склады</a></li>
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('quotes') }}'><i class="las la-user-tie"></i> Квоты</a></li>
 @endif
 <!-- Users, Roles, Permissions -->
 
