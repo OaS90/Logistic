f6b06685139955e8a0dd8221d663f1fb2eb3df35
Aleksandr Ovsyankin <a.ovsyankin@holodilnik.ru>
2023-02-02 13:43:19 +0300

    fix

--- app/Http/Controllers/Api/PartnerController.php
+++ app/Http/Controllers/Api/PartnerController.php
@@ -33,8 +33,10 @@ class PartnerController extends Controller
             $apps = [];
 
             foreach ($applications as $app) {
-                $appDTO = (new PartnerOrderDTO($app))->make();
-                $apps[] = $appDTO;
+                if ($app->status == 'created') {
+                    $appDTO = (new PartnerOrderDTO($app))->make();
+                    $apps[] = $appDTO;
+                }
             }
 
             return response()->json($apps, 200);
