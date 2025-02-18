ab9118782ae790572dcab57a54e6929faee5b291
Aleksandr Ovsyankin <a.ovsyankin@hldtech.ru>
2025-02-18 11:55:10 +0300

    fixes

--- app/Domain/DTO/ProductDTO.php
+++ app/Domain/DTO/ProductDTO.php
@@ -2,28 +2,28 @@
 
 namespace App\Domain\DTO;
 
-class ProductDTO
+readonly class ProductDTO
 {
     /**
      * Последние параметры на данный момент не предусмотрены в форме
      * создания заявки. Возможно в будущем будет доработка.
      */
-    public function __construct(public readonly string $name,
-                                public readonly string $brand,
-                                public readonly string $sku,
-                                public readonly int $count,
-                                public readonly float $cost,
-                                public readonly int $vat,
-                                public readonly float $width,
-                                public readonly float $height,
-                                public readonly float $depth,
-                                public readonly float $volume,
-                                public readonly float $weight,
-                                public readonly ?int $tnved = null,
-                                public readonly ?float $discountCost = null,
-                                public readonly ?string $countryCode = null,
-                                public readonly ?string $barcode = null,
-                                public readonly ?float $leftToPay = null //доплата
+    public function __construct(public string  $name,
+                                public ?string $brand,
+                                public string  $sku,
+                                public int $count,
+                                public float $cost,
+                                public int $vat,
+                                public float $width,
+                                public float $height,
+                                public float $depth,
+                                public float $volume,
+                                public float $weight,
+                                public ?int $tnved = null,
+                                public ?float $discountCost = null,
+                                public ?string $countryCode = null,
+                                public ?string $barcode = null,
+                                public ?float $leftToPay = null //доплата
     )
     {}
 }
