cc9bfd21ffed64e55daafe1153b781129fa2414f
Aleksandr Ovsyankin <a.ovsyankin@hldtech.ru>
2025-12-08 13:02:39 +0300

    DEV-7838 command for generate default settings for shipment warehouses and pvz

---
+++ app/Console/Commands/GenerateStartShipmentConfig.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Hru\Filial;
+use App\Models\TransportCompany;
+use App\Models\TransportCompanyShipmentWarehouse;
+use Illuminate\Console\Command;
+
+class GenerateStartShipmentConfig extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:generate-start-shipment-config';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Генерирует стандартный конфиг для складов отгрузки (departure_point_id Код пвз) и
+        дополнительные настройки.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $settings = config('shipment_pickpoint');
+
+        foreach ($settings as $tcName => $transportSettings) {
+            $tc = TransportCompany::query()->where('code', $tcName)->first();
+
+            if ($tc === null) {
+                $this->info('Не найдена ТК ' . $tcName);
+                continue;
+            }
+
+            foreach ($transportSettings['prod'] as $transportSetting) {
+                $branchOffice = Filial::query()
+                    ->where('filial_id', $transportSetting['branch_office_id'])
+                    ->first();
+
+                if ($branchOffice) {
+                    $tcShipmentSettings = TransportCompanyShipmentWarehouse::query()
+                        ->where('filial_id', $branchOffice->id)
+                        ->where('tc_id', $tc->id)
+                        ->first();
+
+
+                    if (!$tcShipmentSettings) {
+                        $data = [
+                            'filial_id' => $branchOffice->id,
+                            'tc_id' => $tc->id,
+                            'departure_id' => $transportSetting['pvzcode'],
+                        ];
+
+                        $data['data'] = $this->prepareJsonData($transportSetting);
+                        TransportCompanyShipmentWarehouse::query()->create($data);
+                    } else {
+                        $data['data'] = $this->prepareJsonData($transportSetting);
+                        $tcShipmentSettings->update($data);
+                    }
+                } else {
+                    $this->info('Branch office not found ' . $transportSetting['branch_office_id']);
+                }
+            }
+        }
+    }
+
+    private function prepareJsonData(array $settings): array
+    {
+        $preparedData = [];
+        unset($settings['pvzcode']);
+        unset($settings['branch_office_id']);
+        foreach ($settings as $key => $value) {
+            $preparedData[] = [
+                'key' => $key,
+                'value' => $value,
+            ];
+        }
+
+        return $preparedData;
+    }
+}
